4228fc2d0c1c2f18fc8e87dd26bd924ddb58b5ab
jdaeira <jdaeira@gmail.com>
2020-05-07 01:14:25 +0000

    added more scripts

--- Project1/luser-demo01.sh
+++ Project1/luser-demo01.sh
@@ -3,11 +3,42 @@
 # This script displays various information to the screen
 
 # Display 'Hello'
-
 echo 'Hello'
 
-uptime
+# Assign a value to a variable
+WORD='script'
 
-WORD='John'
+# Display that value using the variable
+echo "$WORD"
 
-echo $WORD
+# Demonstrate that single quotes cause variables to NOT get expanded
+echo '$WORD'
+
+# Combine the variable with hard coded text
+echo "This is a shell $WORD"
+
+# Display the contents of the variable using an alternative syntax
+echo "This is a shell ${WORD}"
+
+# Append text to the variable. Need curly braces if adding text after variable 
+echo "{$WORD}ing is fun!"
+
+# Show how NOT to append text to a variable
+# This doesn't work
+echo "$WORDing is fun!"
+
+# Create a new variable
+ENDING='ed'
+
+# Combine the two variables
+echo "This is ${WORD}${ENDING}!"
+
+# Chaange the value stored in the ENDING variable (Reassignment)
+ENDING='ing'
+
+# Combine the two variables
+echo "${WORD}${ENDING} is fun!"
+
+# Reassign value to ENDING
+ENDING='s'
+echo "You are going to write many ${WORD}${ENDING} in this class!"
---
+++ Project1/luser-demo02.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Display the UID aand username of the user executing this script
+# Display if the user is the root user or not
+
+# Display the UID
+echo "Your UID is ${UID}"
+
+# Display the username
+whoami
+
+# Display if the user is root user or not
