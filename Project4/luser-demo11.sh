97fe0b55339394d1572f7775f44548ec49cf693f
jdaeira <jdaeira@gmail.com>
2020-05-30 02:25:02 +0000

    added more scripts from tutorials

--- Project4/luser-demo11.sh
+++ Project4/luser-demo11.sh
@@ -43,6 +43,15 @@ do
     esac    
 done
 
+# Remove the options while leaving the remaining arguments
+shift "$(( OPTIND - 1 ))"
+
+if [[ "${#}" -gt 0  ]]
+then
+  echo "From OPTIND"
+  usage
+fi
+
 log "Generating a password"
 
 PASSWORD=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c${LENGTH})
